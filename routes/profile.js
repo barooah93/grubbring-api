e0102ce085ff548ecbf54a5d3a7b06dd7454593b
urvesh <urveshpatel94@yahoo.com>
2015-10-04 01:03:57 +0000

    added update email route

--- routes/profile.js
+++ routes/profile.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express.Router();
 var debug = require('debug')('grubbring:profile');
+var pool = require('../config/dbconnection.js').pool;
 
 app.get('/',function(req,res){
 	debug(req.method + ' ' + req.url);
@@ -8,8 +9,33 @@ app.get('/',function(req,res){
 		isAuthenticated:req.isAuthenticated(),
 		user: req.user
 	});
-
 });
 
+app.post('/updateEmail', function(req, res) {
+	debug(req.method + ' ' + req.url);
+	
+	var currEmail = req.user.emailAddr;
+	var newEmail = req.body.newEmail;
+	
+	if (!newEmail || currEmail === newEmail) {
+		debug('Email field was empty or the same email address was entered');
+	} else {
+		pool.getConnection(function(err, connection) {
+			if (err) {
+				console.log(err);
+			} else {
+				connection.query('UPDATE tblUser SET emailAddr=? WHERE emailAddr=?', [newEmail, currEmail], function(err, rows) {
+					if (err) {
+						console.log(err);
+					} else {
+						debug('Updated email address');
+					}
+				})
+				connection.release();
+			}
+		})
+	}
+	res.redirect('back');
+})
 
 module.exports = app;
