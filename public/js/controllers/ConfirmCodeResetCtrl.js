674755f9accb074d1b26b0d9d6c129ddbeb9705c
Urvesh <urveshpatel94@yahoo.com>
2015-11-07 17:47:47 -0500

    setup separate controller files

---
+++ public/js/controllers/ConfirmCodeResetCtrl.js
@@ -0,0 +1,7 @@
+/**
+ * Created by Urvesh on 11/7/15.
+ */
+
+angular.module('controllers').controller('ConfirmCodeResetCtrl', function($scope, $http, $location, passEmailService, passAccessCode){
+
+});
---
+++ public/js/controllers/DashboardCtrl.js
@@ -0,0 +1,7 @@
+/**
+ * Created by Urvesh on 11/7/15.
+ */
+
+angular.module('controllers').controller('DashboardCtrl', function DashboardCtrl ($scope, $http, $location) {
+
+});
