279cc0089b4792d2c0ce797b914093a040a1e15f
mz90 <uncharteredstars@gmail.com>
2015-11-10 03:37:40 +0000

    Worked on some of activities.js

--- routes/activities.js
+++ routes/activities.js
@@ -7,10 +7,13 @@ var db = require('../dbexecute');
 var auth = require('../servicesAuthenticate');
 
 /** variables **/
+var user;
+var array = [];
 
 
-
-/**TODO:
+/**
+ * 
+ * TODO:
 1. GET A list of activities (both active and expired) that the user either initiated or was a part of
 2. POST Create activity option where user will create order activity for certain ring
 3. GET Search activities - allows you to search current and expired activites that user initiated or was a part of
@@ -18,10 +21,29 @@ var auth = require('../servicesAuthenticate');
     
 **/
 
+//Gets a list of the activities (both active and expired) that the user initiated or was a part of. List will be from most active to least active
+//FIELDS: ActivityId, Ring name, bringerId, max number of orders, remaining number of orders (indicates whether an activity is open or closed, with the "I'm IN!" button), and the last activity date/time
 app.get('/', function(req,res){
 	
 		auth.checkAuthentication(req,res, function(data){
     		res.json(req.user);	
+    		
+    		if (res.status == 200){
+    		   user = req.user; 
+    		}
     	});
+    	
+    var query;
+    
+    query = "SELECT tblOrder.orderId AS OrderID, tblOrder.ringId, tblOrder.grubberyId, tblOrder.bringerUserId, tblOrder.lastOrderDateTime, SUM(tblOrder.maxNumOrders - tblOrderUser.quantity) " +
+				"AS remainingOrders FROM tblOrder, tblRingUser, tblOrderUser WHERE tblRingUser.userId = '"+user+"' AND tblOrder.ringId = tblRingUser.ringId " +
+				"AND tblOrderUser.orderId = tblOrder.orderId GROUP BY tblOrder.orderId ";
+				
+				db.dbExecuteQuery(q, res, function(result){
+					for (var i = 0; i < result.data.length; i++){
+					    
+					}
+					
+				});
     	
 });
