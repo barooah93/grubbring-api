b6684ec0e236ece58e26a9bfe2ea95c520de5969
mz90 <uncharteredstars@gmail.com>
2016-02-11 02:00:56 +0000

    Cleaned up some code in index.html, got rid of extra stylesheets

---
+++ routes/orders.js
@@ -0,0 +1,6 @@
+//tblOrder - activities table and tblOrderUser - orders within activites
+
+//1. get all orders related to a specific activity
+//2. get all orders related to all activities in a ring
+//3. post a new order to a specific activity
+//4. update order api call
