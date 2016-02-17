5fa2491405d1f44d5230b52b16e88294bbcc281f
sshah0930 <sshah0930@gmail.com>
2016-02-17 03:05:28 +0000

    create order api

---
+++ routes/orders.js
@@ -0,0 +1,61 @@
+var express = require('express');
+var app = express.Router();
+var debug = require('debug')('grubbring:profile');
+//var pool = require('../config/dbconnection.js').pool;
+var encrypt = require('../config/passwordEncryption.js');
+var authenticate = require('../servicesAuthenticate');
+var crypto = require('crypto');
+var db = require('../dbexecute');
+var mysql = require('mysql');
+var emailServices = require('../emailServices');
+var accountAcc = require('../accountAccessibility');
+
+
+//table order user - each unique order for a user
+//table order - 
+
+//create new order for a specific activity
+app.post('/createOrder',function(req,res){
+	authenticate.checkAuthentication(req,res,function(data){
+	    var sql = null;
+	    
+		var orderId = req.body.orderId; //activity Id
+		var userId = req.body.userId;
+		var orderedOn = req.body.orderedOn;
+		var itemOrdered = req.body.itemOrdered;
+		var quantity = req.body.quantity;
+		var comment = req.body.comment;
+		var cost = req.body.cost;
+		var paymentMethod = req.body.paymentMethod;
+		var paymentStatus = req.body.paymentStatus;
+		
+		sql = "INSERT INTO tblOrderUser (orderId, userId, orderedOn, itemOrdered, quantity, comment, cost, paymentMethod, paymentStatus)" +
+	"VALUES (?,?,?,?,?,?,?,?,?)";
+		
+		var inserts = [orderId, userId, orderedOn, itemOrdered, quantity, comment, cost, paymentMethod, paymentStatus];
+		
+		sql = mysql.format(sql, inserts);
+		
+		db.dbExecuteQuery(sql, res, function (result) {
+            result.description = "Order Created.";
+            res.json(result);
+        });
+	});
+});
+
+app.get('/viewAllOrdersForActivity/:activityId', function(req,res) {
+	authenticate.checkAuthentication(req, res, function (data) {
+	    var sql = null;
+	    
+        var activityId = req.params.activityId;
+        
+        sql = "SELECT * FROM tblOrderUser WHERE orderId=?";
+        
+        var inserts = [activityId];
+		
+		
+	});
+});
+
+
+module.exports = app;
