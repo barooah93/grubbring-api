85165c28f755937c20039b789160ed5b3691ea4e
mz90 <uncharteredstars@gmail.com>
2016-05-02 01:15:06 +0000

    Restructured dashboard and find_rings code.

--- public/js/controllers/DashboardCtrl.js
+++ public/js/controllers/DashboardCtrl.js
@@ -3,22 +3,9 @@ angular.module('grubbring.controllers').controller('DashboardCtrl', function Das
     $scope.rings = null;
     $scope.sortedCounts = null;
     getUserDetails();
-    
+    console.log("test userid: "+$scope.userId);
+     getRingsUserIsPartOf();
 
-    // array containing rings near person's location
-    $scope.nearbyRings = [];
-
-    // initialize map canvas
-    var mapCanvas = document.getElementById('map');
-    var zoomLevel = 15;
-
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(successFunction, errorFunction);
-    }
-    else {
-        alert('It seems like Geolocation, which is required for this page, is not enabled in your browser.');
-    }
-    
     // Retrieve user details
     function getUserDetails() {
         $http({
@@ -33,67 +20,7 @@ angular.module('grubbring.controllers').controller('DashboardCtrl', function Das
         });
     }
     
-
-    // if successfully received long and lat
-    function successFunction(position) {
-        // get client coordinates
-        var lat = position.coords.latitude;
-        var long = position.coords.longitude;
-
-        // initialize geocoder for finding long and lat of an address
-        var geocoder = new google.maps.Geocoder();
-
-        // initialize options for map
-        var mapOptions = {
-            center: new google.maps.LatLng(lat, long),
-            zoom: zoomLevel,
-            mapTypeId: google.maps.MapTypeId.ROADMAP
-        }
-
-        // initialize google map object onto div mapCanvas with specified options
-        var map = new google.maps.Map(mapCanvas, mapOptions);
-
-        // decodes address into long and lat coordinates to add markers to the map
-        function codeAddress(ring) {
-            geocoder.geocode({'address': ring.addr + ' ' + ring.city + ', ' + ring.state}, function(results, status) {
-                if (status == google.maps.GeocoderStatus.OK) {
-                    var marker = new google.maps.Marker({
-                        map: map,
-                        position: results[0].geometry.location
-                    });
-                    // add tooltip giving info about the ring
-                    marker.setTitle(ring.name + "\n" + ring.addr + "\n" + ring.firstName + " " + ring.lastName);
-                } else {
-                    alert("We could not find nearby locations successfully: " + status);
-                }
-            });
-
-        }
-
-        // get suggested rings to display to user
-        $http({
-            method: 'GET',
-            url: '/api/ring?latitude='+lat +'&longitude=' + long
-        }).then(function(response) {
-            console.log("users lat long " + lat + " " + long);
-            console.log(response.data.data);
-            if(response.data.data != null) {
-                for (var i = 0; i < response.data.data.length; i++) {
-                    codeAddress(response.data.data[i]);
-                    $scope.nearbyRings.push(response.data.data[i]);
-                }
-            } else {
-                console.log("response.data.data is null - no rings in the area for user " + $scope.userId);
-            }
-
-        }, function(err) {
-            console.log(err);
-        });
-    }
-
-    function errorFunction(position) {
-        console.log('Error!');
-    }
+   
 
     function getRingsUserIsPartOf() { /*broken*/
         $http({
