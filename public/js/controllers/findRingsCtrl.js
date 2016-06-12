43abcdad050fc0893ba07363a8d3e3939bdf9a0e
mz90 <uncharteredstars@gmail.com>
2016-06-12 16:26:06 +0000

    Added info window tooltip to markers - still need to modify marker content

--- public/js/controllers/findRingsCtrl.js
+++ public/js/controllers/findRingsCtrl.js
@@ -67,6 +67,9 @@ angular.module('grubbring.controllers').controller('findRingsCtrl', function fin
         
         // Get nearby rings and display markers
         getNearbyRings($scope.lat, $scope.long);
+        
+        // open popup overlay for selected marker
+        openOverlayOnMapMarkerClick($scope.lat, $scope.long);
 
         // Function to populate list with nearby rings and display markers on map
         function getNearbyRings(lat, long){
@@ -158,6 +161,41 @@ angular.module('grubbring.controllers').controller('findRingsCtrl', function fin
 
         }
         
+         // opens detail popup overlay when clicking on map marker
+        function openOverlayOnMapMarkerClick(marker){
+             var marker = marker;
+             var contentString = '<div id="content">'+
+              '<div id="siteNotice">'+
+              '</div>'+
+              '<h1 id="firstHeading" class="firstHeading">Uluru</h1>'+
+              '<div id="bodyContent">'+
+              '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
+              'sandstone rock formation in the southern part of the '+
+              'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
+              'south west of the nearest large town, Alice Springs; 450&#160;km '+
+              '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
+              'features of the Uluru - Kata Tjuta National Park. Uluru is '+
+              'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
+              'Aboriginal people of the area. It has many springs, waterholes, '+
+              'rock caves and ancient paintings. Uluru is listed as a World '+
+              'Heritage Site.</p>'+
+              '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
+              'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
+              '(last visited June 22, 2009).</p>'+
+              '</div>'+
+              '</div>';
+        
+              var infowindow = new google.maps.InfoWindow({
+                content: contentString
+              });
+        
+
+              marker.addListener('click', function() {
+                infowindow.open(map, marker);
+              });
+            
+        }
+        
         // decodes address into long and lat coordinates to add ring markers to the map
         function placeRingMarkerOnMap(ring) {
             geocoder.geocode({'address': ring.addr + ' ' + ring.city + ', ' + ring.state}, function(results, status) {
@@ -170,12 +208,18 @@ angular.module('grubbring.controllers').controller('findRingsCtrl', function fin
                     marker.setIcon('http://maps.google.com/mapfiles/ms/icons/red-dot.png');
                     // add tooltip giving info about the ring
                     marker.setTitle(ring.name + "\n" + ring.addr + "\n" + ring.firstName + " " + ring.lastName);
+                    // open ring detail popup on marker click
+                    openOverlayOnMapMarkerClick(marker)
+                    
+                    
                 } else {
                     alert("We could not find nearby rings successfully, geocoder could not find location: " + status);
                 }
             });
 
         }
+        
+       
         
         // decodes address into long and lat coordinates to add ring markers to the map
         function placeGrubberyMarkerOnMap(grubbery) {
